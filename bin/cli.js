94b5c88dbeaf34b95b4fc99859eb8dc5c6e5bf56
root <root@jad23-pc.localdomain>
2022-11-30 10:09:44 -0500

    committitng 1

---
+++ bin/cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+import {roll} from "../lib/roll.js"
+
+const vals = minimist(process.argv.slice(2));
+export function main(){
+    console.log(JSON.stringify(roll(vals.sides,vals.dice,vals.rolls)));
+};
+
+main();
